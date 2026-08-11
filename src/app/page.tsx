38de286506eabf0f1ee61c5a9d4c9139cc85b3fd
ALUNO SENAI PETROLINA <aluno.senaipetrolina@SISTEMAFIEPE.LOCAL>
2026-08-11 08:59:31 -0300

    subindo sistema

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+const produtos = [
+	{
+		id:1,
+		nome:"Notebook",
+		preco:4500
+	},
+	{
+		id:2,
+		nome:"Mouse",
+		preco:120
+	},
+	{
+		id:3,
+		nome:"Teclado",
+		preco:250
+	}
+]
+
+export default function Home(){
+	return(
+		<main>
+			<h1>Cloud Produtos</h1>
+			<p>Catalogo de produtos</p>
+			<button>Novo Produto</button>
+			<section>
+				{produtos.map((produto)=>(
+					<div key={produto.id}>
+						<h2>{produto.nome}</h2>
+						<p>R${produto.preco}</p>
+					</div>
+				))}
+			</section>
+		</main>
+	)
+}
